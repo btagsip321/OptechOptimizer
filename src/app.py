06e34609b1ab73ed3856cd67a121d2e220cb2684
btagsip321 <121241058+btagsip321@users.noreply.github.com>
2023-01-12 08:25:03 -0500

    Added more data + app.py changes

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask
+import json
 from buildpc import *
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -8,7 +9,10 @@ app = Flask(__name__)
 def hello():
     return 'Sample web app'
 
-#@app.route('/build')(buildBudget)
+@app.route('/build', methods=['GET'])
+def build():
+    info = json.loads(request.data)
+    jsonify(buildBudget(info.budget, info.windows))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
