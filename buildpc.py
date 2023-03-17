089a835e0d312c8a29ea9c5f7185f2946ed8e5e7
Brael Tagsip <121241058+btagsip321@users.noreply.github.com>
2023-03-17 09:44:07 -0400

    Reformatted and rescraped data, added loading screen, added sliders

--- buildpc.py
+++ buildpc.py
@@ -3,7 +3,7 @@ import json
 import pdb
 import os
 
-parts = ['CPU','GPU','HDD','RAM','SSD', 'CASE']
+parts = ['CPU','GPU','HDD','RAM','SSD','CASE']
 pc_parts = {'CPU':None, 'GPU':None, 'HDD':None, 'RAM':None, 'SSD':None, 'CASE':None}
 
 def cleanBudget(budget, minBudget =0, maxBudget =1000):
@@ -15,18 +15,20 @@ def cleanBudget(budget, minBudget =0, maxBudget =1000):
 def buildBudget(budget, windows = False):
     subtr = (100 if windows else 10)
     budget -= subtr
-    if((budget * .05) < 45):
+
+    if ((budget * .05) < 45):
         budget -= 45
-        caseMoney = round((45), 2)
+        caseMoney = 45
     else:
         caseMoney = round((budget * .05), 2)
-    if((budget * .051 < 51)):
+
+    if ((budget * .051 < 51)):
         budget -= 51
         ssdMoney = 51
     else:
         ssdMoney = round((budget * .051), 2)
     
-    budget = cleanBudget(budget, 1000, 15000)
+    budget = cleanBudget(budget, 800, 15000)
 
     return {
         "GPU": cleanBudget((budget * 0.306), 0, 5000),
@@ -44,41 +46,23 @@ def buildBudget(budget, windows = False):
     }
 
 def gatherPartData(part):
-    path = os.path.join('./old_data/', part + '_UserBenchmarks.csv')
+    path = os.path.join('./data/', part + '.csv')
     df = pd.read_csv(path).dropna()
+    pc_parts[part] = df[df.Price != -1]
+    print(pc_parts[part])
     
-    if(part == "SSD" or part == "HDD"):
-        for(index, modelNo) in df["Model"].items():
-            if("GB" in modelNo):
-                place = modelNo.index("GB")
-                threeBefore = place - 3
-                if(modelNo[threeBefore:place].isdigit):
-                    df.at[index, "SizeGB"] = modelNo[threeBefore:place]
-            elif("TB" in modelNo):
-                place = modelNo.index("TB")
-                twoBefore = place - 2
-                oneBefore = place - 1
-                if(modelNo[twoBefore:place].isdigit):
-                    df.at[index, "SizeGB"] = int(modelNo[twoBefore:place]) * 1000
-                elif(modelNo[oneBefore:place].isdigit):
-                    df.at[index, "SizeGB"] = int(modelNo[oneBefore:place]) * 1000
-    pc_parts[part] = df[pd.to_numeric(df['Price'], errors='coerce').notnull()]
-
-
 def findPartsWithinBudget(part, budget, preferredBrand, ssdStorageSpace, hddStorageSpace):
     part_data = pc_parts[part]
 
     if preferredBrand:
-        part_data = part_data[part_data["Brand"] == preferredBrand]
+        part_data = part_data[part_data["Name"] in preferredBrand]
     if ssdStorageSpace:
-        part_data = part_data[part_data["SizeGB"].astype(int) >= ssdStorageSpace]
+        part_data = part_data[part_data["Capacity"].astype(int) >= ssdStorageSpace]
     if hddStorageSpace:
-        part_data = part_data[part_data["SizeGB"].astype(int) >= hddStorageSpace]
+        part_data = part_data[part_data["Capacity"].astype(int) >= hddStorageSpace]
     
-    part_data["Price"] = pd.to_numeric(part_data['Price'])
-    part_data = part_data.sort_values(by=['Rank'], ascending=True)
-    updated_part_data = part_data[part_data["Price"] <= budget]
-    return updated_part_data.iloc[0]['Brand'] + " " + updated_part_data.iloc[0]['Model'] + " Part Number: " + updated_part_data.iloc[0]['Part Number'] + " Price: " + str(updated_part_data.iloc[0]["Price"]) + " Buy Here: " + updated_part_data.iloc[0]["URL"]
+    part_data = part_data[part_data["Price"] <= budget]
+    return part_data.iloc[0]['Name']
 
 def buildPc(budget, cpu, ssdStorageSpace, hddStorageSpace):
     return {
@@ -96,4 +80,3 @@ def buildPc(budget, cpu, ssdStorageSpace, hddStorageSpace):
 for part in parts:
     print("Gathering part data for", part)
     gatherPartData(part)
-#buildPc(buildBudget(1000, False), None, 100, 100)
--- src/webscrape.py
+++ src/webscrape.py
@@ -3,43 +3,82 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import pandas as pd
+import time
 import pdb
 import os
 import re
+import glob
 
-scraper = webdriver.Firefox()
-pc_parts = {}
+# Selenium web driver scraper. Uses firefox since this is the only browser that works on my laptop
+SCRAPER = webdriver.Firefox()
 
-parts = {
-    "GPU": "https://gpu.userbenchmark.com/",
-    "CPU": "https://cpu.userbenchmark.com/",
-    "SSD": "https://ssd.userbenchmark.com/",
-    "HDD": "https://hdd.userbenchmark.com/",
-    "RAM": "https://ram.userbenchmark.com/",
+# List of parts to loop through, with the part category as the key and the URL as the value
+PARTS = {
+    "GPU": ["https://gpu.userbenchmark.com/", {"Rank":0, "Name":1, "Price":7}],
+    "CPU": ["https://cpu.userbenchmark.com/", {"Rank":0, "Name":1, "Price":9}],
+    "RAM": ["https://ram.userbenchmark.com/", {"Rank":0, "Name":1, "Price":9}],
+    "SSD": ["https://ssd.userbenchmark.com/", {"Rank":0, "Name":1, "Price":9, "Capacity":5}],
+    "HDD": ["https://hdd.userbenchmark.com/", {"Rank":0, "Name":1, "Price":9, "Capacity":5}],
 }
 
+# Number of iterations, amount of entries = 50 + iterations*50
+ITERATIONS = 2
+
+# Attempts to fetch the price
+# If it fails to do so, returns -1 to signal an error
 def fetch_price(raw_price):
     try:
         return int(raw_price.split("\n")[0][1:].replace(",",""))
     except:
         return -1
+    
+# Attempts to fetch the capacity, converts to GB
+def fetch_capacity(raw_capacity):
+    capacity = raw_capacity.split("\n")[0]
+    return int(''.join(i for i in capacity if i.isdigit())) * (1000 if "TB" in raw_capacity else 1)
+        
+print("Scraping data...")
 
+# Loop through each part
+for part in PARTS:
 
+    #Create a pd dataframe to be turned into a CSV
+    index = PARTS[part]
+    url, format = index[0], index[1]
+    csv = pd.DataFrame(columns = format.keys())
 
-for part in parts:
-    csv = pd.DataFrame(columns = ['Rank', 'Name', 'Price'])
+    # Send a get request to URL
+    SCRAPER.get(url)
 
-    scraper.get(parts[part])
-    mytable = scraper.find_element(By.CSS_SELECTOR, '#tableDataForm\:mhtddyntac > table')
-    for row in mytable.find_elements(By.CSS_SELECTOR, 'tr'):
-        elements = row.find_elements(By.TAG_NAME, 'td')
-        if len(elements) > 0:
-            csv.loc[len(csv.index)] = [
-                int(elements[0].text),
-                elements[1].text.split("\n")[1],
-                fetch_price(elements[7].text)
-            ]
-    print(csv)
-    #csv.to_csv()
+    # Loop through all iterations
+    for i in range(0, ITERATIONS):
 
-#pdb.set_trace()
+        # Loop through table elements
+        mytable = SCRAPER.find_element(By.CSS_SELECTOR, '#tableDataForm\:mhtddyntac > table')
+        for row in mytable.find_elements(By.CSS_SELECTOR, 'tr'):
+            elements = row.find_elements(By.TAG_NAME, 'td')
+            if len(elements) > 0:
+
+                # Initiate new row variable
+                newRow = [
+                    int(elements[format["Rank"]].text), #Rank
+                    elements[format["Name"]].text.split("\n")[1], #Name
+                    fetch_price(elements[format["Price"]].text), #Price
+                ]
+
+                # Capacity, only for SSD and HDD
+                if (part == "SSD" or part == "HDD"):
+                    capacity = fetch_capacity(elements[format["Capacity"]].text)
+                    newRow.append(capacity)
+
+                # Add row to the csv dataframe
+                csv.loc[len(csv.index)] = newRow
+
+        # Click the next button to go through next page on the table
+        SCRAPER.find_element(By.CSS_SELECTOR, "#tableDataForm\:j_idt274").click()
+        time.sleep(4)
+        
+    # Create new CSV file
+    file = open("../data/" + part + ".csv", "a")
+    file.write(csv.to_csv(encoding='utf-8', index=False))
+    file.close()
