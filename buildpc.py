1676b1fed777589bc003fa39d5b1236650cab5dd
Gabriel Sternberg <121241991+GabeSternberg@users.noreply.github.com>
2023-05-04 10:19:38 -0400

    Undid testing error

--- buildpc.py
+++ buildpc.py
@@ -7,7 +7,7 @@ import re
 parts = ['CPU','GPU','HDD','RAM','SSD','CASE']
 cpuMax = 1400
 gpuMax = 1800
-ssdPref = False
+ssdPref = True
 pc_parts = {'CPU':None, 'GPU':None, 'HDD':None, 'RAM':None, 'SSD':None, 'CASE':None}
 #Rounds budget to 2 decimal places
 def cleanBudget(budget, minBudget =0, maxBudget =50000):
